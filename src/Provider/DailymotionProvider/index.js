2f3d650c129cd99cdb65cf1234a84f40b07b1521
Davide Gheri <davide.gheri@docomodigital.com>
2019-01-08 10:11:36 +0100

    DailymotionProvider methods, async SDK loading

--- src/Provider/DailymotionProvider/index.js
+++ src/Provider/DailymotionProvider/index.js
@@ -1,20 +1,27 @@
 import loadScript from 'load-script2';
+import { getDomNode, Unsupported } from '../../lib';
 
 const sdkCdn = 'https://api.dmcdn.net/all.js';
 let sdk = null;
 
-export class DailymotionProvider {
+@Unsupported('off')
+export default class DailymotionProvider {
     id = null;
+
+    domNodeId = null;
 
     dmPlayer = null;
 
-    isMuted = false;
-
-    isFullScreen = false;
-
     listeners = {};
 
-    parent = null;
+    ready = null;
+
+    constructor(options, id) {
+        this.id = id;
+
+        this.ready = this.createDM(options.domNode,
+            Object.assign({}, { video: options.videoId }, options.providerOptions || {}));
+    }
 
     loadSDK() {
         if (!sdk) {
@@ -35,22 +42,79 @@ export class DailymotionProvider {
         return sdk;
     }
 
-    getDomNode(domNode) {
-        if (typeof domNode === 'string') {
-            if (domNode.indexOf('#') === 0) {
-                return document.querySelector(domNode);
-            }
-            return document.getElementById(domNode);
-        }
-        return domNode;
+    createDM(domNode, options) {
+        return new Promise((resolve, reject) => {
+            this.loadSDK().then(() => {
+                domNode = getDomNode(domNode);
+                this.domNodeId = domNode.id;
+                this.dmPlayer = window.DM.player(domNode, options);
+                this.dmPlayer.addEventListener('apiready', () => resolve());
+            }).catch(err => reject(err));
+        });
     }
 
-    constructor(options, id) {
-        this.id = id;
+    clear() {
+        document.getElementById(this.domNodeId).remove();
+    }
 
+    on(event, cb) {
+        this.ready.then(() => {
+            this.dmPlayer.addEventListener(event, cb);
+            this.listeners[event] = cb;
+        });
+    }
 
-        console.log(options);
+    play() {
+        this.ready.then(() => this.dmPlayer.play());
+    }
+
+    pause() {
+        this.ready.then(() => this.dmPlayer.pause());
+    }
+
+    stop() {
+        this.ready.then(() => {
+            this.dmPlayer.pause();
+            this.dmPlayer.seek(0);
+        });
+    }
+
+    mute() {
+        this.ready.then(() => this.dmPlayer.setMuted(true));
+    }
+
+    unmute() {
+        this.ready.then(() => this.dmPlayer.setMuted(false));
+    }
+
+    toggleMute() {
+        this.ready.then(() => this.dmPlayer.toggleMuted());
+    }
+
+    toggleFullScreen() {
+        this.ready.then(() => this.dmPlayer.setFullscreen(!this.dmPlayer.fullscreen));
+    }
+
+    setVolume(volumeLevel) {
+        if (volumeLevel > 1) {
+            volumeLevel /= 100;
+        }
+        this.ready.then(() => this.dmPlayer.setVolume(volumeLevel));
+    }
+
+    forward(seconds) {
+        this.ready.then(() => this.dmPlayer.seek(this.dmPlayer.currentTime + seconds));
+    }
+
+    rewind(seconds) {
+        this.ready.then(() => this.dmPlayer.seek(this.dmPlayer.currentTime - seconds));
+    }
+
+    seek(seconds) {
+        this.ready.then(() => this.dmPlayer.seek(seconds));
+    }
+
+    getListeners() {
+        return this.listeners;
     }
 }
-
-export default DailymotionProvider;
